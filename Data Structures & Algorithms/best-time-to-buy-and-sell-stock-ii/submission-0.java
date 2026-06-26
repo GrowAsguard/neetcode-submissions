febfe0b6eb8aca2e6198d47314c7a0465fceaa75
Kabir Ahmed <68875748+GrowAsguard@users.noreply.github.com>
2026-06-27 01:12:21 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int profit = 0;
+        for (int price : prices) {
+            if (price < buy) buy = price;
+            else {
+                profit += price - buy;
+                buy = price;
+            }
+        }
+        return profit;
+    }
+}
