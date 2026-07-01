abc68a0dcc9bc9beff84ee70eee63b4e6ee1693d
Kabir Ahmed <68875748+GrowAsguard@users.noreply.github.com>
2026-07-02 02:12:07 +0530

    Add: find-k-closest-elements - submission-2

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-2.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        List<Integer> result = new ArrayList<>();
+        int n = arr.length;
+        
+        int closeLeft = 0;
+        int closeRight = 0;
+
+        if (arr[n - 1] < x) {
+            closeRight = n-1;
+            closeLeft = n-2;
+        }
+        else if (arr[0] > x) {
+            closeLeft = 0;
+            closeRight = 1;
+        }
+        else {
+            while (closeRight < n-1 && arr[closeRight] < x) {
+                closeRight++;
+            }
+            closeLeft = closeRight - 1;
+        }
+
+        for (int i=0; i<k; i++) {
+
+            if (closeLeft < 0) {
+                result.add(arr[closeRight]);
+                closeRight++;
+            }
+            else if (closeRight == n) {
+                result.add(0, arr[closeLeft]);
+                closeLeft--;
+            }
+            else if (closeLeft >= 0 && Math.abs(arr[closeLeft] - x) < Math.abs(arr[closeRight] - x) ) {
+                result.add(0, arr[closeLeft]);
+                closeLeft--;
+            } 
+            else if (closeRight < n && Math.abs(arr[closeLeft] - x) > Math.abs(arr[closeRight] - x)) {
+                result.add(arr[closeRight]);
+                closeRight++;
+            }
+            else{
+                result.add(0, arr[closeLeft]);
+                closeLeft--;
+            }
+        }
+        return result;
+    }
+}
