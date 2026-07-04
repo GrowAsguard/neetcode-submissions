23868f806ef77d88c816d8e843162ea5bdf20bd6
Kabir Ahmed <68875748+GrowAsguard@users.noreply.github.com>
2026-07-04 23:54:25 +0530

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque();
+
+        Map<Character, Character> brackets = new HashMap<>();
+        brackets.put(']', '[');
+        brackets.put(')', '(');
+        brackets.put('}', '{');
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') stack.push(c);
+            else {
+                if (stack.peek() != brackets.get(c)) return false;
+                else stack.pop();
+            }
+        }
+
+        if (!stack.isEmpty()) return false;
+        else return true;
+    }
+}
